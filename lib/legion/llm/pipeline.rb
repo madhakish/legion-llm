e3165bd86c74a104649a4350d8497231d64a3c7f
Esity <matthewdiverson@gmail.com>
2026-03-23 19:13:36 -0500

    wire Pipeline module into Legion::LLM requires

---
+++ lib/legion/llm/pipeline.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'pipeline/request'
+require_relative 'pipeline/response'
+require_relative 'pipeline/profile'
+
+module Legion
+  module LLM
+    module Pipeline
+    end
+  end
+end
