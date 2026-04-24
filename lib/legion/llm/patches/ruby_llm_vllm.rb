541a93d29ff14a6e834c47c8e8037bfe03d4d931
Esity <matthewdiverson@gmail.com>
2026-04-24 01:20:09 -0500

    feat: add RubyLLM vLLM provider class

---
+++ lib/legion/llm/patches/ruby_llm_vllm.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module RubyLLM
+  module Providers
+    class Vllm < OpenAI
+      def api_base
+        @config.vllm_api_base
+      end
+
+      def headers
+        return {} unless @config.vllm_api_key
+
+        { 'Authorization' => "Bearer #{@config.vllm_api_key}" }
+      end
+
+      class << self
+        def configuration_options
+          %i[vllm_api_base vllm_api_key]
+        end
+
+        def configuration_requirements
+          %i[vllm_api_base]
+        end
+
+        def local?
+          true
+        end
+
+        def capabilities
+          nil
+        end
+      end
+    end
+  end
+end
+
+RubyLLM::Provider.register :vllm, RubyLLM::Providers::Vllm
